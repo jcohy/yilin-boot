e29ccb51e31e24248a375812b9e483b7cdd25e4a
Jcohy <jia_chao23@126.com>
2023-07-26 14:43:25 +0800

    1、添加实体

---
+++ yilin-reactive-starters/yilin-reactive-starter-jpa/src/test/java/com/yilin/reactive/starter/jpa/domain/Customer.java
@@ -0,0 +1,29 @@
+package com.yilin.reactive.starter.jpa.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * 描述: .
+ *
+ * <p>
+ * Copyright © 2023 <a href="https://www.jcohy.com" target= "_blank">https://www.jcohy.com</a>
+ *
+ * @author jiac
+ * @version 1.0.0 2023/7/25 18:23
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "user")
+public class Customer extends BaseDomain {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
