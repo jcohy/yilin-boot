89ca8d6d1c36832f5c5f4a2c24a0e97b0e13bc22
Jcohy <jia_chao23@126.com>
2023-09-15 09:40:24 +0800

    1、add multiLock

---
+++ yilin-reactive-starters/yilin-reactive-starter-redis/src/test/java/com/yilin/reactive/starter/redis/AbstractRedisReactiveTestSupport.java
@@ -0,0 +1,63 @@
+package com.yilin.reactive.starter.redis;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+/**
+ * Copyright: Copyright (c) 2023 <a href="https://www.jcohy.com" target="_blank">jcohy.com</a>
+ * <p> Description:
+ *
+ * @author jiac
+ * @version 2023.0.1 2023/9/6:14:53
+ * @since 2023.0.1
+ */
+@Testcontainers(disabledWithoutDocker = true)
+@DataRedisTest
+public class AbstractRedisReactiveTestSupport {
+
+	@Container
+	static RedisContainer redis = new RedisContainer();
+
+	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+	private final RedissonReactiveClient redissonClient;
+
+
+	public AbstractRedisReactiveTestSupport(ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
+		this.reactiveRedisTemplate = reactiveRedisTemplate;
+		this.redissonClient = Redisson.create(config()).reactive();
+	}
+
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+	}
+
+	static Config config() {
+		var config = new Config();
+		var address = redis.getHost() + ":" + redis.getFirstMappedPort();
+		address = address.startsWith("redis://") ? address : "redis://" + address;
+		SingleServerConfig serverConfig = config.useSingleServer()
+				.setAddress(address);
+		return config;
+	}
+
+	public ReactiveRedisTemplate<String, String> getReactiveRedisTemplate() {
+		return reactiveRedisTemplate;
+	}
+
+	public RedissonReactiveClient getRedissonClient() {
+		return redissonClient;
+	}
+}
--- yilin-reactive-starters/yilin-reactive-starter-redis/src/test/java/com/yilin/reactive/starter/redis/ReactiveStringKeyRedisTemplateTest.java
+++ yilin-reactive-starters/yilin-reactive-starter-redis/src/test/java/com/yilin/reactive/starter/redis/ReactiveStringKeyRedisTemplateTest.java
@@ -4,13 +4,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.redisson.Redisson;
 import reactor.core.publisher.Hooks;
 import reactor.test.StepVerifier;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.connection.BitFieldSubCommands.BitFieldGet;
 import org.springframework.data.redis.connection.BitFieldSubCommands.BitFieldIncrBy;
@@ -22,6 +20,7 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+
 /**
  * Copyright: Copyright (c) 2023 <a href="https://www.jcohy.com" target="_blank">jcohy.com</a>
  * <p> Description:
@@ -30,13 +29,8 @@ import org.springframework.test.context.DynamicPropertySource;
  * @version 2023.0.1 2023/8/18:16:03
  * @since 2023.0.1
  */
-@Testcontainers(disabledWithoutDocker = true)
-@DataRedisTest
-public class ReactiveStringKeyRedisTemplateTest {
 
-
-	@Container
-	static RedisContainer redis = new RedisContainer();
+public class ReactiveStringKeyRedisTemplateTest extends AbstractRedisReactiveTestSupport {
 
 	static {
 		Hooks.onOperatorDebug();
@@ -44,14 +38,13 @@ public class ReactiveStringKeyRedisTemplateTest {
 
 	private final ReactiveStringKeyRedisTemplate<String, String> template;
 
-	@Autowired
-	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
 	@Autowired
 	public ReactiveStringKeyRedisTemplateTest(ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
-		this.reactiveRedisTemplate = reactiveRedisTemplate;
-		this.template = new ReactiveStringKeyRedisTemplate<>(reactiveRedisTemplate);
+		super(reactiveRedisTemplate);
+		this.template = new ReactiveStringKeyRedisTemplate<>(reactiveRedisTemplate, Redisson.create().reactive());
 	}
+
 
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
@@ -63,7 +56,7 @@ public class ReactiveStringKeyRedisTemplateTest {
 	void scan() {
 
 		Map<String, String> tuples = new LinkedHashMap<>(Map.of("k1", "v1", "k2", "v2", "k3", "v3", "k4", "v4", "k5", "v5", "k6", "v6", "k7", "v7", "k8", "v8", "k9", "v9", "k10", "v10"));
-		reactiveRedisTemplate.opsForValue().multiSet(tuples)
+		template.stringOps().stringMultiSet(tuples)
 				.as(StepVerifier::create)
 				.expectNext(true)
 				.verifyComplete();
